883f4b2701768ea3621a445bac0142509bb2a502
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2017-03-03 23:01:00 +0900

    Minor improvements to fakepriv

--- april_fakepriv/init.lua
+++ april_fakepriv/init.lua
@@ -5,32 +5,13 @@ local fakepriv_timeout = 300 -- 5 mins
 local fakepriv_chance = 10 -- one out of ten
 local fakeprivs = { 'stoptime', 'steal' }
 
-local function sleepwalk(player)
-	local pos = player:getpos()
-	local newpos = pos
-	local random_number = math.random(4)
-	if random_number == 1 then
-		newpos.x = newpos.x + 1
-	elseif random_number == 2 then
-		newpos.x = newpos.x - 1
-	elseif random_number == 3 then
-		newpos.z = newpos.z + 1
-	else
-		newpos.z = newpos.z - 1
-	end
-	-- if there's enough space for the player and the node below is walkable
-	if minetest.get_node(pos).name == "air" and minetest.get_node({x = pos.x, y = pos.y + 1, z = pos.z}).name == "air" and minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z}).walkable == true then
-		player:moveto(newpos)
-	end
-end
-
 local function timer()
 	local connected_players = minetest.get_connected_players()
-	if connected_players == nil then
+	local nr_connected_players = table.getn(connected_players)
+	if nr_connected_players == nil then
 		minetest.after(fakepriv_timeout, timer)
 		return
 	end
-	local nr_connected_players = #connected_players
 	--we need at least two active players to run the prank
 	if nr_connected_players > 1 then
 		if math.random(fakepriv_chance) == 1 then
